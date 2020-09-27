9645e9bda4a8de53b1d4d7f4a583b06cd4c8d3f1
soongle2 <skql5810@gmail.com>
2020-09-27 17:58:09 +0900

    change parameter in plan.h

--- plan.h
+++ plan.h
@@ -11,12 +11,12 @@ private:
 	int meal_type;
 public:
 	Plan();
-	Plan(Date _date, Meal _menu, int _meal_type);
+	Plan(Date& _date, Meal& _menu, int _meal_type);
 	~Plan();
 	Date get_date();
-	void set_date(Date _date);
+	void set_date(Date& _date);
 	Meal get_menu();
-	void set_menu(Meal _menu);
+	void set_menu(Meal& _menu);
 	int get_meal_type();
 	void set_meal_type(int _meal_type);
 	void ShowPlan();
