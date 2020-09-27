335e7a81cb0a42daddda6bbf87d169f283d928c2
soongle2 <skql5810@gmail.com>
2020-09-27 17:05:14 +0900

    change getter and setter

--- plan.h
+++ plan.h
@@ -13,12 +13,12 @@ public:
 	Plan();
 	Plan(Date _date, Meal _menu, int _meal_type);
 	~Plan();
-	Date get_date(Date _date);
-	void set_date();
-	Meal get_menu(Meal _menu);
-	void set_menu();
+	Date get_date();
+	void set_date(Date _date);
+	Meal get_menu();
+	void set_menu(Meal _menu);
 	int get_meal_type();
-	void set_meal_type();
+	void set_meal_type(int _meal_type);
 	void ShowPlan();
 };
 #endif
