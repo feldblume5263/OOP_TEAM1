a25fd2ea7dc60f877ab809f0a3151be2f8334106
feldblume5263 <feldblume5263@gmail.com>
2020-09-29 18:23:23 +0900

    update with implemenntation without sorting

--- 5_PlanManager/PlanManager.cpp
+++ 5_PlanManager/PlanManager.cpp
@@ -1,4 +1,4 @@
-#include "PlanManger.h"
+#include "PlanManager.h"
 
 using namespace std;
 
@@ -10,27 +10,68 @@ PlanManger::~PlanManger()
 {
 }
 
-void    PlanManger::add_plan()
+void	PlanManger::add_plan(Plan planToAdd)
 {
-
+	plan_array.push_back(planToAdd);
 }
 
-void    PlanManger::delete_plan()
+void	PlanManger::delete_plan(int _month, int _day, int _meal_type)
 {
+	int		idx;
 
+	idx = 0;
+	while (idx < plan_array.size())
+	{
+		if (plan_array[idx].find_plan(_month, _day, _meal_type) == true)
+		{
+			plan_array.erase(plan_array.begin() + idx);
+			return ;
+		}
+		idx++;
+	}
+	cout << "No matching Plans" << endl;
+	return ;
 }
 
-void    PlanManger::revise_plan()
+void	PlanManger::revise_plan(int _month, int _day, int _meal_type)
 {
+	Plan	planToRevise;
 
+	delete_plan(_month, _day, _meal_type);
+	planToRevise.make_plan(); //plan클래스에서 plan만드는 단계
+	add_plan(planToRevise);
 }
 
-void    PlanManger::show_all_meal()
+void	PlanManger::show_all_meal()
 {
+	int		idx;
 
+	idx = 0;
+	while (idx < plan_array.size())
+	{
+		plan_array[idx].print_plan();
+		idx++;
+	}
+	return ;
 }
 
-void    PlanManger::find_by_date()
+int		main(void)
 {
+	Plan	plan1(7, 21, "hamburger", 2);
+	Plan	plan2(4, 12, "cake", 3);
+	Plan	plan3(1, 7, "chicken", 1);
+
+	PlanManger plan_data;
+	plan_data.add_plan(plan1);
+	plan_data.add_plan(plan2);
+	plan_data.add_plan(plan3);
+	cout << "<before delete>" << endl;
+	plan_data.show_all_meal();
+	plan_data.delete_plan(7, 21, 2);
+	cout << "<after delete>" << endl;
+	plan_data.show_all_meal();
+
+
+
 
 }
--- 5_PlanManager/PlanManager.h
+++ 5_PlanManager/PlanManager.h
@@ -1,18 +1,57 @@
 #ifndef PLANMANAGER_H
 # define PLANMANAGER_H
 
+//# include "plan.h"
 # include <iostream>
 # include <vector>
+# include <algorithm>
+# include <cstring>
 
 using namespace std;
 
 class               Plan
 {
 private:
-    int             date;
-    string          meal;
-    int             meal_type;
+    int             month = 0;
+    int             day = 0;
+    string          meal = "";
+    int             meal_type = 0;
 
+public:
+    Plan(int _month, int _day, string _meal, int _meal_type)
+    {
+        month = _month;
+        day = _day;
+        meal = _meal;
+        meal_type = _meal_type;
+    }
+    Plan() {}
+
+    void    make_plan() {}
+
+    bool    find_plan(int _month, int _day, int _meal_type)
+    {
+        if (month == _month && day == _day && meal_type == _meal_type)
+            return (true);
+        return (false);
+    }
+
+    void    print_plan()
+    {
+        string      meal_type_string;
+
+        if (meal_type == 1)
+            meal_type_string = "Breakfast";
+        else if (meal_type == 2)
+            meal_type_string = "Lunch";
+        else if (meal_type == 3)
+            meal_type_string = "Dinner";
+        else
+            meal_type_string = "unknown";
+        cout << month << "/" << day << "/" << meal_type_string << endl;
+        cout << meal << endl;
+        cout << endl;
+    }
 };
 
 class               PlanManger
@@ -24,11 +63,11 @@ public:
     PlanManger();
     ~PlanManger();
 
-    void            add_plan();
-    void            delete_plan();
-    void            revise_plan();
+    void            add_plan(Plan planToAdd);
+    void            delete_plan(int _month, int _day, int _meal_type);
+    void            revise_plan(int _month, int _day, int _meal_type);
     void            show_all_meal();
-    void            find_by_date();
+    bool            find_plan(Plan planToFind, int _month, int _day, int _meal_type);
 };
 
 #endif
