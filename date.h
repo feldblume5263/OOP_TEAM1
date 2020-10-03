7bb7e091ecd810476944951ff5183c0929ab35e2
soongle2 <skql5810@gmail.com>
2020-10-03 11:06:39 +0900

    fix date.h

--- date.h
+++ date.h
@@ -13,9 +13,9 @@ public:
 	Date(int _month, int _day, string _comment);
 	~Date();
 	int get_month();
-	void set_month(int_month);
+	void set_month(int _month);
 	int get_day();
-	void set_month(int_day);
+	void set_day(int _day);
 	string get_comment();
 	void set_comment(string _comment);
 };
